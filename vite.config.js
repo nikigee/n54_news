6978c81f265b463a968cda1cd75d3a6161d116e1
Nikita Golev <ngolev.bus@gmail.com>
2024-12-12 13:04:52 +1100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV == "production" ? '/n54_news/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
